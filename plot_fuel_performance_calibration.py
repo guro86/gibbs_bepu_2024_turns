11dd386f7fe4804999f55c86ad72d5f3d4970fd3
JOSEPH MURE <joseph.mure@edf.fr>
2024-08-22 18:27:44 +0200

    Exploration

--- plot_fuel_performance_calibration.py
+++ plot_fuel_performance_calibration.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 
 ot.RandomGenerator.SetSeed(0)
 
+ot.ResourceMap.SetAsUnsignedInteger("RandomWalkMetropolisHastings-DefaultBurnIn", 10000)
+
 # %%
 # Load the models
 import os
@@ -139,6 +141,18 @@ class PosteriorParametersSigmaSquare(ot.OpenTURNSPythonFunction):
         post_lambda = 2.0 / np.sum(squares)  # rate lambda =  1 / beta
         post_k = nexp / 2.0  # shape
 
+        print("dim = ", self._dim)
+        print([post_lambda, post_k, self._lb, self._ub])
+        print("empirical standard deviation = ",  np.sqrt(np.mean(squares)))
+        trinvgamma = ot.TruncatedDistribution(ot.InverseGamma(), 0.0, 1.0)
+        try:
+            trinvgamma.setParameter([post_lambda, post_k, self._lb, self._ub])
+        except:
+            print("DID NOT WORK")
+            print([post_lambda, post_k, self._lb, self._ub])
+            invgamma = ot.InverseGamma(post_lambda, post_k)
+            print(invgamma.computeCDF(float(self._lb)))
+            print(invgamma.computeCDF(float(self._ub)))
         # Hyperparameters of a truncated inverse gamma
         return [post_lambda, post_k, self._lb, self._ub]
 
@@ -199,7 +213,7 @@ ubs = [40.0, 10.0, 1.0]
 
 # %%
 # Lower and upper bounds for :math:`\sigma_{diff}^2, \sigma_{gb\_satutation}^2, \sigma_{crack}^2`
-lbs_sigma_square = np.array([0.1, 0.1, 0.1]) ** 2
+lbs_sigma_square = np.array([0.1, 0.00001, 0.1]) ** 2
 ubs_sigma_square = np.array([40, 20, 10]) ** 2
 
 # %%
@@ -210,7 +224,7 @@ initial_x = np.repeat([[19.0, 4.0, 0.4]], repeats=nexp, axis=0).flatten().tolist
 initial_state = initial_mus + initial_sigma_squares + initial_x
 
 
-initial_state = [7.25437,5.34333,0.2688,16.0572,0.0400135,0.0272261,11.9245,5.37405,0.525344,5.73046,5.38231,0.0911716,9.63216,5.24496,0.374362,14.0424,5.75832,0.271396,9.87644,5.03691,0.162653,9.82311,5.39674,0.390882,8.00765,5.09337,0.16456,2.65465,5.69722,0.394443,2.5703,5.23133,0.426503,5.8409,5.17407,0.0137091,7.6153,5.28807,0.192897,6.22031,5.09896,0.170041,11.6519,4.92299,0.286779,3.35373,5.27964,0.334533,7.88469,5.57834,0.41805,2.37791,5.35143,0.224999,8.20026,5.54231,0.454754,9.27286,5.4026,0.126513,9.47674,5.26752,0.376011,7.66051,4.99955,0.240198,6.52532,5.35201,0.268828,8.92401,5.2735,0.6278,7.10376,5.37863,0.190223,9.51549,5.62299,0.22081,4.92388,5.45614,0.330533,5.33167,5.2495,0.165869,7.62818,5.45446,0.0581072,10.2911,5.17247,0.23252,14.6922,5.15172,0.731592,0.369771,4.72366,0.20958,11.0594,5.43281,0.063014]
+initial_state = [7.51188,8.51937,0.292952,9.37161,0.0516549,0.0290076,9.08868,8.45879,0.48073,6.34297,8.5543,0.150879,7.93481,8.65959,0.519287,4.31882,8.39354,0.525883,6.85787,8.70104,0.189518,7.52134,9.01983,0.422947,9.55284,8.76941,0.125033,6.16268,8.03187,0.165988,6.23355,8.53377,0.427718,1.34126,8.12576,0.463676,6.8223,8.25217,0.215145,4.32857,8.48588,0.235282,4.16995,8.64144,0.439799,12.6397,8.79266,0.493053,7.17511,9.2374,0.432577,6.79614,8.72714,0.184218,9.13748,8.19622,0.476752,4.74385,8.38604,0.189267,6.50082,8.48837,0.356241,10.7832,8.43997,0.251964,10.0711,7.98261,0.200379,16.7423,8.86075,0.527558,9.69518,8.77775,0.166641,8.56429,8.69221,0.199346,15.5955,8.38788,0.272872,10.2809,8.48477,0.132925,10.5913,8.3798,0.0507148,8.156,8.56329,0.237119,7.92834,8.12394,0.536364,1.47742,8.88654,0.161173,4.48622,8.71523,0.230233]
 
 # %%
 # Support of the prior (and thus posterior) distribution
@@ -269,14 +283,14 @@ for exp in range(nexp):
             ot.Function(PosteriorLogDensityX(exp=exp)),
             support,
             initial_state,
-            ot.Normal([0.0] * 3, [20.0, 5.0, 0.5]),
+            ot.Normal([0.0] * 3, [6.0, 0.5, 0.15]),
             [base_index + i for i in range(ndim)],
         )
     ]
 
 # %%
 # Run all samplers independently once to make sure they work.
-sampler_realizations = [s.getRealization() for s in samplers]
+#sampler_realizations = [s.getRealization() for s in samplers]
 
 # %%
 # The Gibbs algorithm combines all these samplers.
@@ -291,10 +305,15 @@ sampler.setDescription(mu_desc + sigma_square_desc + x_desc)
 # Run this Metropolis-within-Gibbs algorithm and check the acceptance rates
 # for the Random walk Metropolis-Hastings samplers.
 
-samples = sampler.getSample(2000)
+samples = sampler.getSample(20000)
 acceptance = [
     sampler.getMetropolisHastingsCollection()[i].getAcceptanceRate()
-    for i in range(len(samplers))
+    for i in range(6, len(samplers))
+]
+
+adaptation_factor = [
+    sampler.getMetropolisHastingsCollection()[i].getImplementation().getAdaptationFactor()
+    for i in range(6, len(samplers))
 ]
 
 print("Minimum acceptance rate = ", np.min(acceptance))
@@ -304,7 +323,9 @@ print("Maximum acceptance rate for random walk MH = ", np.max(acceptance[2 * ndi
 # Represent the last sampled points (i.e. those which are least dependent on the initial state)
 # We are only interested in the :math:`\mu` and :math:`\sigma` parameters.
 
-reduced_samples = samples[1000:, 0:6]
+#reduced_samples = samples[samples.getSize()*3//4::20, 0:6]
+#reduced_samples = samples[0::100, 0:6]
+reduced_samples = samples[:, 0:6]
 
 # %%
 # It is possible to quickly draw pair plots.
@@ -350,7 +371,6 @@ ot.ResourceMap.SetAsString("Contour-DefaultColorMap", "viridis")
 for i in range(1, full_grid.getNbRows()):
     for j in range(i):
         graph = full_grid.getGraph(i, j)
-        bb = graph.getBoundingBox()
         cloud = graph.getDrawable(0).getImplementation()
         cloud.setPointStyle(".")
         data = cloud.getData()
@@ -358,7 +378,7 @@ for i in range(1, full_grid.getNbRows()):
         contour = dist.drawPDF().getDrawable(0).getImplementation()
         contour.setLevels(np.linspace(0.0, contour.getLevels()[-1], 10))
         graph.setDrawables([contour, cloud])
-        graph.setBoundingBox(bb)
+        graph.setBoundingBox(contour.getBoundingBox())
         full_grid.setGraph(i, j, graph)
 
 _ = View(full_grid, scatter_kw={"alpha": 0.2})
@@ -416,6 +436,17 @@ plt.xlabel("Measurements")
 plt.ylabel("Prediction ranges")
 
 plt.show()
+
+# %%
+from copulogram import Copulogram
+import pandas as pd
+
+reduced_df = pd.DataFrame(np.array(reduced_samples))
+reduced_df.columns = [name for name in reduced_samples.getDescription()]
+reduced_df["INDEX"] = reduced_df.index
+#reduced_df.columns = reduced_samples.getDescription()
+copulogram = Copulogram(reduced_df)
+copulogram.draw(alpha=0.25, marker=".", hue="INDEX")
 
 
 # %%
