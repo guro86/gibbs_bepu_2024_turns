8b44414524f9c292565feb158be19833fd52e351
JOSEPH MURE <joseph.mure@edf.fr>
2024-08-22 19:02:31 +0200

    Remove Copulogram

--- plot_fuel_performance_calibration.py
+++ plot_fuel_performance_calibration.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 ot.RandomGenerator.SetSeed(0)
 
-#ot.ResourceMap.SetAsUnsignedInteger("RandomWalkMetropolisHastings-DefaultBurnIn", 10000)
+# ot.ResourceMap.SetAsUnsignedInteger("RandomWalkMetropolisHastings-DefaultBurnIn", 10000)
 
 # %%
 # Load the models
@@ -21,21 +21,23 @@ import os
 
 if "gibbs_bepu_2024_turns" in os.getcwd():
     from fuel_performance import FuelPerformance
+
     fp = FuelPerformance()
 else:
     from openturns.usecases import fuel_performance
+
     fp = fuel_performance.FuelPerformance()
-ndim = fp.Xtrain.getDimension() # dimension of the model inputs: 3
-desc = fp.Xtrain.getDescription() # description of the model inputs (diff, crack)
-nexp = fp.ytrain.getDimension() # number of experiments (each has a specific model)
-models = fp.models # the nexp models
+ndim = fp.Xtrain.getDimension()  # dimension of the model inputs: 3
+desc = fp.Xtrain.getDescription()  # description of the model inputs (diff, crack)
+nexp = fp.ytrain.getDimension()  # number of experiments (each has a specific model)
+models = fp.models  # the nexp models
 
 # %%
 # Each experiment :math:`i` produced one measurement value,
 # which is used to define the likelihood of the associated model :math:`\mathcal{M}_i`
 # and latent variable :math:`\vect{x}_i = (x_{i, diff}, x_{i, crack})`.
 
-likes = fp.likes 
+likes = fp.likes
 
 # %%
 # Random vector to sample the conditional posterior
@@ -58,6 +60,7 @@ sigma_square_desc = ["$\\sigma$_{{{}}}^2".format(label) for label in desc]
 # - the parameters of the conditional posterior distributions of the :math:`\mu` parameters
 # - the parameters of the conditional posterior distributions of the :math:`\sigma` parameters
 # - the conditional posterior log-PDF of the latent variables.
+
 
 class PosteriorParametersMu(ot.OpenTURNSPythonFunction):
     """Outputs the parameters of the conditional posterior distribution of one
@@ -141,19 +144,6 @@ class PosteriorParametersSigmaSquare(ot.OpenTURNSPythonFunction):
         post_lambda = 2.0 / np.sum(squares)  # rate lambda =  1 / beta
         post_k = nexp / 2.0  # shape
 
-        # print("dim = ", self._dim)
-        # print([post_lambda, post_k, self._lb, self._ub])
-        # print("empirical standard deviation = ",  np.sqrt(np.mean(squares)))
-        # trinvgamma = ot.TruncatedDistribution(ot.InverseGamma(), 0.0, 1.0)
-        # try:
-        #     trinvgamma.setParameter([post_lambda, post_k, self._lb, self._ub])
-        # except:
-        #     print("DID NOT WORK")
-        #     print([post_lambda, post_k, self._lb, self._ub])
-        #     invgamma = ot.InverseGamma(post_lambda, post_k)
-        #     print(invgamma.computeCDF(float(self._lb)))
-        #     print(invgamma.computeCDF(float(self._ub)))
-        # Hyperparameters of a truncated inverse gamma
         return [post_lambda, post_k, self._lb, self._ub]
 
 
@@ -223,13 +213,11 @@ initial_sigma_squares = [20.0 ** 2, 0.5 ** 2]
 initial_x = np.repeat([[19.0, 0.4]], repeats=nexp, axis=0).flatten().tolist()
 initial_state = initial_mus + initial_sigma_squares + initial_x
 
-
-#initial_state = [7.51188,8.51937,0.292952,9.37161,0.0516549,0.0290076,9.08868,8.45879,0.48073,6.34297,8.5543,0.150879,7.93481,8.65959,0.519287,4.31882,8.39354,0.525883,6.85787,8.70104,0.189518,7.52134,9.01983,0.422947,9.55284,8.76941,0.125033,6.16268,8.03187,0.165988,6.23355,8.53377,0.427718,1.34126,8.12576,0.463676,6.8223,8.25217,0.215145,4.32857,8.48588,0.235282,4.16995,8.64144,0.439799,12.6397,8.79266,0.493053,7.17511,9.2374,0.432577,6.79614,8.72714,0.184218,9.13748,8.19622,0.476752,4.74385,8.38604,0.189267,6.50082,8.48837,0.356241,10.7832,8.43997,0.251964,10.0711,7.98261,0.200379,16.7423,8.86075,0.527558,9.69518,8.77775,0.166641,8.56429,8.69221,0.199346,15.5955,8.38788,0.272872,10.2809,8.48477,0.132925,10.5913,8.3798,0.0507148,8.156,8.56329,0.237119,7.92834,8.12394,0.536364,1.47742,8.88654,0.161173,4.48622,8.71523,0.230233]
-
 # %%
 # Support of the prior (and thus posterior) distribution
 support = ot.Interval(
-    lbs + lbs_sigma_square.tolist() + nexp * lbs, ubs + ubs_sigma_square.tolist() + nexp * ubs
+    lbs + lbs_sigma_square.tolist() + nexp * lbs,
+    ubs + ubs_sigma_square.tolist() + nexp * ubs,
 )
 
 # %%
@@ -263,7 +251,9 @@ samplers += [
         initial_state,
         [ndim + i],
         ot.Function(
-            PosteriorParametersSigmaSquare(dim=i, lb=lbs_sigma_square[i], ub=ubs_sigma_square[i])
+            PosteriorParametersSigmaSquare(
+                dim=i, lb=lbs_sigma_square[i], ub=ubs_sigma_square[i]
+            )
         ),
     )
     for i in range(ndim)
@@ -290,7 +280,8 @@ for exp in range(nexp):
 
 # %%
 # Run all samplers independently once to make sure they work.
-#sampler_realizations = [s.getRealization() for s in samplers]
+
+# sampler_realizations = [s.getRealization() for s in samplers]
 
 # %%
 # The Gibbs algorithm combines all these samplers.
@@ -312,7 +303,9 @@ acceptance = [
 ]
 
 adaptation_factor = [
-    sampler.getMetropolisHastingsCollection()[i].getImplementation().getAdaptationFactor()
+    sampler.getMetropolisHastingsCollection()[i]
+    .getImplementation()
+    .getAdaptationFactor()
     for i in range(6, len(samplers))
 ]
 
@@ -323,8 +316,6 @@ print("Maximum acceptance rate for random walk MH = ", np.max(acceptance[2 * ndi
 # Represent the last sampled points (i.e. those which are least dependent on the initial state)
 # We are only interested in the :math:`\mu` and :math:`\sigma` parameters.
 
-#reduced_samples = samples[samples.getSize()*3//4::20, 0:6]
-#reduced_samples = samples[0::100, 0:6]
 reduced_samples = samples[:, 0:4]
 
 # %%
@@ -341,7 +332,7 @@ for i in range(pair_plots.getNbRows()):
         pair_plots.setGraph(i, j, graph)
 
 _ = View(pair_plots)
-       
+
 # %%
 # Create an enhanced pair plots grid with histograms of the marginals on the diagonal.
 
@@ -358,7 +349,7 @@ for i in range(pair_plots.getNbRows()):
     for j in range(pair_plots.getNbColumns()):
         if len(pair_plots.getGraph(i, j).getDrawables()) > 0:
             full_grid.setGraph(i + 1, j, pair_plots.getGraph(i, j))
-    
+
 _ = View(full_grid)
 
 
@@ -388,7 +379,9 @@ _ = View(full_grid, scatter_kw={"alpha": 0.1})
 # Retrieve the :math:`\vect{\mu}` and :math:`\vect{\sigma}^2` columns in the sample.
 
 mu = samples.getMarginal(["$\\mu$_{{{}}}".format(label) for label in desc])
-sigma_square = np.sqrt(samples.getMarginal(["$\\sigma$_{{{}}}^2".format(label) for label in desc]))
+sigma_square = np.sqrt(
+    samples.getMarginal(["$\\sigma$_{{{}}}^2".format(label) for label in desc])
+)
 
 
 # %%
@@ -399,7 +392,10 @@ sigma_square = np.sqrt(samples.getMarginal(["$\\sigma$_{{{}}}^2".format(label) f
 # corresponding to the posterior samples of the :math:`\mu` and :math:`\sigma` parameters.
 
 truncation_interval = ot.Interval(lbs, ubs)
-normal_collection = [ot.TruncatedDistribution(ot.Normal(mean, std), truncation_interval) for (mean, std) in zip(mu, sigma_square)]
+normal_collection = [
+    ot.TruncatedDistribution(ot.Normal(mean, std), truncation_interval)
+    for (mean, std) in zip(mu, sigma_square)
+]
 normal_mixture = ot.Mixture(normal_collection)
 normal_mixture.setDescription(desc)
 
@@ -437,17 +433,6 @@ plt.xlabel("Measurements")
 plt.ylabel("Prediction ranges")
 
 plt.show()
-
-# %%
-from copulogram import Copulogram
-import pandas as pd
-
-reduced_df = pd.DataFrame(np.array(reduced_samples))
-reduced_df.columns = [name for name in reduced_samples.getDescription()]
-reduced_df["INDEX"] = reduced_df.index
-#reduced_df.columns = reduced_samples.getDescription()
-copulogram = Copulogram(reduced_df)
-copulogram.draw(alpha=0.25, marker=".", hue="INDEX")
 
 
 # %%
